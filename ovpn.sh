56aac5d83b9c2ae56fbbbc7fd74be1c7bd5d4a93
Ishrak <642351+ishrak1010@users.noreply.github.com>
2017-10-23 21:13:06 +0600

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -140,13 +140,13 @@ if [[ -e /etc/openvpn/server.conf ]]; then
 				else
 					IP=$(grep 'iptables -t nat -A POSTROUTING -s 10.4.0.0/24 ! -d 10.4.0.0/24 -j SNAT --to ' $RCLOCAL | cut -d " " -f 14)
 					iptables -t nat -D POSTROUTING -s 10.4.0.0/24 ! -d 10.4.0.0/24 -j SNAT --to $IP
-					sed -i '/iptables -t nat -A POSTROUTING -s 10.8.0.0\/24 ! -d 10.8.0.0\/24 -j SNAT --to /d' $RCLOCAL
+					sed -i '/iptables -t nat -A POSTROUTING -s 10.4.0.0\/24 ! -d 10.4.0.0\/24 -j SNAT --to /d' $RCLOCAL
 					if iptables -L -n | grep -qE '^ACCEPT'; then
 						iptables -D INPUT -p $PROTOCOL --dport $PORT -j ACCEPT
 						iptables -D FORWARD -s 10.4.0.0/24 -j ACCEPT
 						iptables -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 						sed -i "/iptables -I INPUT -p $PROTOCOL --dport $PORT -j ACCEPT/d" $RCLOCAL
-						sed -i "/iptables -I FORWARD -s 10.8.0.0\/24 -j ACCEPT/d" $RCLOCAL
+						sed -i "/iptables -I FORWARD -s 10.4.0.0\/24 -j ACCEPT/d" $RCLOCAL
 						sed -i "/iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT/d" $RCLOCAL
 					fi
 				fi
@@ -256,7 +256,7 @@ key server.key
 dh dh.pem
 auth SHA1
 topology subnet
-server 10.8.0.0 255.255.255.0
+server 10.4.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 	echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
 	# DNS
